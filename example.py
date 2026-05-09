b2745738c12c8ece46bd85bbda8ad67e0f0ae7f1
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2026-05-09 13:12:43 +0200

    Update to latest llvm-nanobind

--- example.py
+++ example.py
@@ -3,13 +3,11 @@ import llvm
 if __name__ == "__main__":
     with llvm.create_context() as ctx:
         with ctx.create_module("example_module") as mod:
-            func_type = ctx.function_type(
-                ctx.int32_type(), [ctx.int32_type(), ctx.int32_type()]
-            )
+            types = ctx.types
+            func_type = types.function(types.i32, [types.i32, types.i32])
             add_func = mod.add_function("add", func_type)
-            entry = add_func.append_basic_block("entry", ctx)
-            with ctx.create_builder() as builder:
-                builder.position_at_end(entry)
+            entry = add_func.append_basic_block("entry")
+            with entry.create_builder() as builder:
                 a, b = add_func.params
                 a.name = "a"
                 b.name = "b"
